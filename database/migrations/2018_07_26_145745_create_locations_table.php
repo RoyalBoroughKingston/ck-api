771b2b59e6f90d10570b8064f072659bd4b52e1f
Matt Inamdar <matt@ayup.agency>
2018-07-26 15:59:42 +0100

    added locations migration

---
+++ database/migrations/2018_07_26_145745_create_locations_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('address_line_1');
+            $table->string('address_line_2')->nullable();
+            $table->string('address_line_3')->nullable();
+            $table->string('city');
+            $table->string('county');
+            $table->string('postcode');
+            $table->string('country');
+            $table->decimal('lat', 9, 6);
+            $table->decimal('lon', 9, 6);
+            $table->text('accessibility_info')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
