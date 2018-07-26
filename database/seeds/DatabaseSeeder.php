31f18d5c100f666a402bd82ffe9991230a9a463e
Matt Inamdar <matt@ayup.agency>
2018-07-26 16:48:35 +0100

    added seeder for taxonomies table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(RolesTableSeeder::class);
         $this->call(ReportTypesTableSeeder::class);
+        $this->call(TaxonomiesTableSeeder::class);
     }
 }
