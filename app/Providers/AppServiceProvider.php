b254ce6c788fb355029eb7ca6e48bae22d3d7718
Matt Inamdar <matt@ayup.agency>
2018-07-26 15:02:33 +0100

    added organisations table

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Blueprint::macro('foreignUuid', function (string $column, string $referencesTable, string $referencedColumn = 'id', bool $nullable = false) {
+            $this->uuid($column)->nullable($nullable);
+            $this->foreign($column)->references($referencedColumn)->on($referencesTable);
+        });
+
+        Blueprint::macro('nullableForeignUuid', function (string $column, string $referencesTable, string $referencedColumn = 'id') {
+            $this->foreignUuid($column, $referencesTable, $referencedColumn, true);
+        });
     }
 
     /**
