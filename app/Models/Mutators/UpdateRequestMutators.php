7ff4f1615e810066ec6fd3530ae0dd397cce78d4
Matt Inamdar <matt@ayup.agency>
2018-07-27 14:00:25 +0100

    added update request mutators

--- app/Models/Mutators/UpdateRequestMutators.php
+++ app/Models/Mutators/UpdateRequestMutators.php
@@ -4,5 +4,20 @@ namespace App\Models\Mutators;
 
 trait UpdateRequestMutators
 {
-    //
+    /**
+     * @param string $data
+     * @return array
+     */
+    public function getDataAttribute(string $data): array
+    {
+        return json_decode($data, true);
+    }
+
+    /**
+     * @param array $data
+     */
+    public function setDataAttribute(array $data)
+    {
+        $this->attributes['data'] = json_encode($data);
+    }
 }
