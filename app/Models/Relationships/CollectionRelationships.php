40adce2405d1801def556e94238e2315500b3468
Matt Inamdar <matt@ayup.agency>
2018-07-27 13:05:34 +0100

    added collection relationships

--- app/Models/Relationships/CollectionRelationships.php
+++ app/Models/Relationships/CollectionRelationships.php
@@ -2,7 +2,24 @@
 
 namespace App\Models\Relationships;
 
+use App\Models\CollectionTaxonomy;
+use App\Models\Taxonomy;
+
 trait CollectionRelationships
 {
-    //
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function collectionTaxonomies()
+    {
+        return $this->hasMany(CollectionTaxonomy::class);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasManyThrough
+     */
+    public function taxonomies()
+    {
+        return $this->hasManyThrough(Taxonomy::class, CollectionTaxonomy::class);
+    }
 }
