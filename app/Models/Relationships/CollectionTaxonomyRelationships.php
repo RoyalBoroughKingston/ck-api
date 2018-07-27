9cbdb3d7ab0e4c966110c2b92344688d01623b54
Matt Inamdar <matt@ayup.agency>
2018-07-27 13:06:43 +0100

    added collection taxonomy relationships

--- app/Models/Relationships/CollectionTaxonomyRelationships.php
+++ app/Models/Relationships/CollectionTaxonomyRelationships.php
@@ -2,7 +2,24 @@
 
 namespace App\Models\Relationships;
 
+use App\Models\Collection;
+use App\Models\Taxonomy;
+
 trait CollectionTaxonomyRelationships
 {
-    //
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function collection()
+    {
+        return $this->belongsTo(Collection::class);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function taxonomy()
+    {
+        return $this->belongsTo(Taxonomy::class);
+    }
 }
