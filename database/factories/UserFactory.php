d753c4bd9a093329de824f479d0aeac134e5d00a
Matt Inamdar <matt@ayup.agency>
2018-07-27 15:43:39 +0100

    removed docblock

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,17 +3,6 @@
 use App\Models\User;
 use Faker\Generator as Faker;
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
-
 $factory->define(User::class, function (Faker $faker) {
     return [
         'first_name' => $faker->firstName,
