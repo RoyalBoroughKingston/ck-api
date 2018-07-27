7d6e0e7f9df5184f16c0d935f6071e0222fb950c
Matt Inamdar <matt@ayup.agency>
2018-07-27 13:05:26 +0100

    added audit relationships

--- app/Models/Relationships/AuditRelationships.php
+++ app/Models/Relationships/AuditRelationships.php
@@ -2,7 +2,15 @@
 
 namespace App\Models\Relationships;
 
+use App\Models\User;
+
 trait AuditRelationships
 {
-    //
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
