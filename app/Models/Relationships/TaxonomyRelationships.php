968cc77f74edc06cd18db339571d3a5cd174420b
Matt Inamdar <matt@ayup.agency>
2018-07-30 11:48:08 +0100

    added placeholder category and persona collection seeders

--- app/Models/Relationships/TaxonomyRelationships.php
+++ app/Models/Relationships/TaxonomyRelationships.php
@@ -2,6 +2,8 @@
 
 namespace App\Models\Relationships;
 
+use App\Models\Taxonomy;
+
 trait TaxonomyRelationships
 {
     /**
@@ -10,5 +12,13 @@ trait TaxonomyRelationships
     public function parent()
     {
         return $this->belongsTo(static::class, 'parent_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function children()
+    {
+        return $this->hasMany(Taxonomy::class, 'parent_id');
     }
 }
