bc4a38239fef6e520d5e425001b706e2490ce8b2
Matt Inamdar <matt@ayup.agency>
2018-08-06 13:10:19 +0100

    added tests for unauthorised user roles

--- tests/Feature/ReportSchedulesTest.php
+++ tests/Feature/ReportSchedulesTest.php
@@ -2,7 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Models\Organisation;
+use App\Models\Service;
+use App\Models\User;
 use Illuminate\Http\Response;
+use Laravel\Passport\Passport;
 use Tests\TestCase;
 
 class ReportSchedulesTest extends TestCase
@@ -16,6 +20,45 @@ class ReportSchedulesTest extends TestCase
         $response = $this->json('GET', '/core/v1/report-schedules');
 
         $response->assertStatus(Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function test_service_worker_cannot_list_them()
+    {
+        $service = factory(Service::class)->create();
+        $user = factory(User::class)->create();
+        $user->makeServiceWorker($service);
+
+        Passport::actingAs($user);
+
+        $response = $this->json('GET', '/core/v1/report-schedules');
+
+        $response->assertStatus(Response::HTTP_FORBIDDEN);
+    }
+
+    public function test_service_admin_cannot_list_them()
+    {
+        $service = factory(Service::class)->create();
+        $user = factory(User::class)->create();
+        $user->makeServiceAdmin($service);
+
+        Passport::actingAs($user);
+
+        $response = $this->json('GET', '/core/v1/report-schedules');
+
+        $response->assertStatus(Response::HTTP_FORBIDDEN);
+    }
+
+    public function test_organisation_admin_cannot_list_them()
+    {
+        $organisation = factory(Organisation::class)->create();
+        $user = factory(User::class)->create();
+        $user->makeOrganisationAdmin($organisation);
+
+        Passport::actingAs($user);
+
+        $response = $this->json('GET', '/core/v1/report-schedules');
+
+        $response->assertStatus(Response::HTTP_FORBIDDEN);
     }
 
     /*
