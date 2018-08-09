0bb2d189fa1ae8dfcbdc048f193d7c56850cf209
Matt Inamdar <matt@ayup.agency>
2018-08-09 10:18:46 +0100

    added placeholder users.store tests

--- tests/Feature/UsersTest.php
+++ tests/Feature/UsersTest.php
@@ -12,7 +12,9 @@ use Tests\TestCase;
 class UsersTest extends TestCase
 {
     /*
+     * ==================================================
      * List all the users.
+     * ==================================================
      */
 
     public function test_guest_cannot_list_them()
@@ -47,18 +49,165 @@ class UsersTest extends TestCase
     }
 
     /*
+     * ==================================================
      * Create a user.
+     * ==================================================
      */
 
     /*
+     * Guest Invoked.
+     */
+    public function test_guest_cannot_create_one()
+    {
+        $this->markTestIncomplete();
+    }
+
+    /*
+     * Service Worker Invoked.
+     */
+    public function test_service_worker_cannot_create_one()
+    {
+        $this->markTestIncomplete();
+    }
+
+    /*
+     * Service Admin Invoked.
+     */
+    public function test_service_admin_cannot_create_service_worker_for_another_service()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_service_admin_can_create_service_worker_for_their_service()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_service_admin_cannot_create_service_admin_for_another_service()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_service_admin_can_create_service_admin_for_their_service()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_service_admin_cannot_create_organisation_admin()
+    {
+        $this->markTestIncomplete();
+    }
+
+    /*
+     * Organisation Admin Invoked.
+     */
+    public function test_organisation_admin_cannot_create_service_worker_for_another_service()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_organisation_admin_can_create_service_worker_for_their_service()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_organisation_admin_cannot_create_service_admin_for_another_service()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_organisation_admin_can_create_service_admin_for_their_service()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_organisation_admin_cannot_create_organisation_admin_for_another_organisation()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_organisation_admin_can_create_organisation_admin_for_their_organisation()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_organisation_admin_cannot_create_global_admin()
+    {
+        $this->markTestIncomplete();
+    }
+
+    /*
+     * Global Admin Invoked.
+     */
+    public function test_global_admin_can_create_service_worker()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_global_admin_can_create_service_admin()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_global_admin_can_create_organisation_admin()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_global_admin_can_create_global_admin()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_global_admin_cannot_create_super_admin()
+    {
+        $this->markTestIncomplete();
+    }
+
+    /*
+     * Super Admin Invoked.
+     */
+
+    public function test_super_admin_can_create_service_worker()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_super_admin_can_create_service_admin()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_super_admin_can_create_organisation_admin()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_super_admin_can_create_global_admin()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_super_admin_can_create_super_admin()
+    {
+        $this->markTestIncomplete();
+    }
+
+    /*
+     * ==================================================
      * Get a specific user.
+     * ==================================================
      */
 
     /*
+     * ==================================================
      * Update a specific user.
+     * ==================================================
      */
 
     /*
+     * ==================================================
      * Delete a specific user.
+     * ==================================================
      */
 }
