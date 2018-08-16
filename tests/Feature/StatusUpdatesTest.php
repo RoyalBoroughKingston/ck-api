1ee192d479b947627c28a86605d367cc439351a2
Matt Inamdar <matt@ayup.agency>
2018-08-16 10:30:58 +0100

    added audit tests for status update controller

--- tests/Feature/StatusUpdatesTest.php
+++ tests/Feature/StatusUpdatesTest.php
@@ -2,10 +2,13 @@
 
 namespace Tests\Feature;
 
+use App\Events\EndpointHit;
+use App\Models\Audit;
 use App\Models\Referral;
 use App\Models\Service;
 use App\Models\User;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Event;
 use Laravel\Passport\Passport;
 use Tests\TestCase;
 
@@ -48,5 +51,24 @@ class StatusUpdatesTest extends TestCase
             'to' => Referral::STATUS_IN_PROGRESS,
             'comments' => 'Assigned to me',
         ]);
+    }
+
+    public function test_audit_created_when_listed()
+    {
+        $this->fakeEvents();
+
+        $service = factory(Service::class)->create();
+        $user = factory(User::class)->create()->makeServiceWorker($service);
+        $referral = factory(Referral::class)->create(['service_id' => $service->id]);
+
+        Passport::actingAs($user);
+
+        $this->json('GET', "/core/v1/status-updates?filter[referral_id]={$referral->id}");
+
+        Event::assertDispatched(EndpointHit::class, function (EndpointHit $event) use ($user, $referral) {
+            return ($event->getAction() === Audit::ACTION_READ) &&
+                ($event->getUser()->id === $user->id) &&
+                ($event->getModel()->id === $referral->id);
+        });
     }
 }
