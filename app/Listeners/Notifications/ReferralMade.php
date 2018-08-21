f1ad2418fab4e05cbac2cd84e736906e0e540d96
Matt Inamdar <matt@ayup.agency>
2018-08-21 13:29:33 +0100

    added placeholder listener for referral made

---
+++ app/Listeners/Notifications/ReferralMade.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Listeners\Notifications;
+
+use App\Events\EndpointHit;
+use App\Models\Audit;
+use App\Models\Referral;
+
+class ReferralMade
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  EndpointHit  $event
+     * @return void
+     */
+    public function handle(EndpointHit $event)
+    {
+        // Only handle specific endpoint events.
+        if ($event->isntFor(Referral::class, Audit::ACTION_CREATE)) {
+            return;
+        }
+
+        logger()->debug('Referral', $event->getModel()->toArray());
+    }
+}
