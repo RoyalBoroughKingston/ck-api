7f9ee1a1e2b1871d7e48aafe3242835cfd80be93
Matt Inamdar <matt@ayup.agency>
2018-08-29 10:07:41 +0100

    made service id filter optional for referrals

--- app/Http/Requests/Referral/IndexRequest.php
+++ app/Http/Requests/Referral/IndexRequest.php
@@ -14,9 +14,20 @@ class IndexRequest extends FormRequest
      */
     public function authorize()
     {
-        $service = Service::find($this->filter['service_id']);
+        // If the user passed in a filter for the services, then loop through each one and check if authorised.
+        $serviceIdsString = $this->input('filter', [])['service_id'] ?? '';
+        $serviceIds = array_filter(explode(',', $serviceIdsString));
 
-        if ($this->user()->isServiceWorker($service)) {
+        $services = Service::query()->whereIn('id', $serviceIds);
+
+        foreach ($services as $service) {
+            if (!$this->user()->isServiceWorker($service)) {
+                return false;
+            }
+        }
+
+        // Finally check if the user is at least a service worker to access the route.
+        if ($this->user()->isServiceWorker()) {
             return true;
         }
 
@@ -30,8 +41,6 @@ class IndexRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            'filter.service_id' => ['required', 'exists:services,id'],
-        ];
+        return [];
     }
 }
