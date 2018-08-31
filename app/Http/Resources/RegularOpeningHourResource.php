e089b1727c170b8029886dd14e3d4b11121264a9
Matt Inamdar <matt@ayup.agency>
2018-08-31 14:59:35 +0100

    weekday returned with nth occurence of month

--- app/Http/Resources/RegularOpeningHourResource.php
+++ app/Http/Resources/RegularOpeningHourResource.php
@@ -17,7 +17,15 @@ class RegularOpeningHourResource extends JsonResource
     {
         return [
             'frequency' => $this->frequency,
-            'weekday' => $this->when($this->frequency === RegularOpeningHour::FREQUENCY_WEEKLY, $this->weekday),
+            'weekday' => $this->when(
+                in_array($this->frequency,
+                    [
+                        RegularOpeningHour::FREQUENCY_WEEKLY,
+                        RegularOpeningHour::FREQUENCY_NTH_OCCURRENCE_OF_MONTH,
+                    ]
+                ),
+                $this->weekday
+            ),
             'day_of_month' => $this->when($this->frequency === RegularOpeningHour::FREQUENCY_MONTHLY, $this->day_of_month),
             'occurrence_of_month' => $this->when($this->frequency === RegularOpeningHour::FREQUENCY_NTH_OCCURRENCE_OF_MONTH, $this->occurrence_of_month),
             'starts_at' => $this->when($this->frequency === RegularOpeningHour::FREQUENCY_FORTNIGHTLY, optional($this->starts_at)->toDateString()),
