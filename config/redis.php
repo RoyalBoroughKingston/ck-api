8f9f570555dd138db29f024e2055af0458ddef2d
Matt Inamdar <matt@ayup.agency>
2018-09-12 22:47:04 +0100

    specified redis queue and session connections

--- config/redis.php
+++ config/redis.php
@@ -14,6 +14,22 @@ $redis = [
         'database' => 0,
     ],
 
+    'queue' => [
+        'scheme' => env('REDIS_SCHEME', 'tcp'),
+        'host' => env('REDIS_HOST', '127.0.0.1'),
+        'password' => env('REDIS_PASSWORD', null),
+        'port' => env('REDIS_PORT', 6379),
+        'database' => 1,
+    ],
+
+    'session' => [
+        'scheme' => env('REDIS_SCHEME', 'tcp'),
+        'host' => env('REDIS_HOST', '127.0.0.1'),
+        'password' => env('REDIS_PASSWORD', null),
+        'port' => env('REDIS_PORT', 6379),
+        'database' => 2,
+    ],
+
     // For clustered setup.
     'clusters' => [
         'default' => [
@@ -23,6 +39,26 @@ $redis = [
                 'password' => env('REDIS_PASSWORD', null),
                 'port' => env('REDIS_PORT', 6379),
                 'database' => 0,
+            ]
+        ],
+
+        'queue' => [
+            [
+                'scheme' => env('REDIS_SCHEME', 'tcp'),
+                'host' => env('REDIS_HOST', '127.0.0.1'),
+                'password' => env('REDIS_PASSWORD', null),
+                'port' => env('REDIS_PORT', 6379),
+                'database' => 1,
+            ]
+        ],
+
+        'session' => [
+            [
+                'scheme' => env('REDIS_SCHEME', 'tcp'),
+                'host' => env('REDIS_HOST', '127.0.0.1'),
+                'password' => env('REDIS_PASSWORD', null),
+                'port' => env('REDIS_PORT', 6379),
+                'database' => 2,
             ]
         ],
 
@@ -46,6 +82,8 @@ $redis = [
 
 if (env('REDIS_CLUSTER', false)) {
     unset($redis['default']);
+    unset($redis['queue']);
+    unset($redis['session']);
 } else {
     unset($redis['clusters']);
 }
