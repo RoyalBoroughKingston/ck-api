eacccaabdf10100dccd44c0de99e86291fa24433
Matt Inamdar <matt@ayup.agency>
2018-09-13 19:26:28 +0100

    added route caching

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
+# Set variables.
+DOCUMENT_ROOT=/var/www/html
+SECRET_ID=ck-api-env-production
+SECRET_REGION=eu-west-1
+
 # Go to the install directory.
-cd /var/www/html
+cd $DOCUMENT_ROOT
 
 # Download the .env file from AWS Secrets Manager.
 echo "Importing .env file..."
-aws secretsmanager get-secret-value --secret-id ck-api-env-production --region eu-west-1 | \
-python -c "import json,sys;obj=json.load(sys.stdin);print obj['SecretString'];" > .env
+aws secretsmanager get-secret-value --secret-id $SECRET_ID --region $SECRET_REGION | \
+    python -c "import json,sys;obj=json.load(sys.stdin);print obj['SecretString'];" > .env
 
 # Install NPM dependencies.
 npm install
@@ -14,15 +19,16 @@ npm run prod
 
 # Install composer dependencies.
 export COMPOSER_HOME="$HOME/.config/composer/"
-composer install --no-interaction --no-dev
+composer install --no-interaction --no-dev --optimize-autoloader
 
 # Run migrations.
 php artisan migrate --force
 
-# Cache the config.
+# Cache.
 php artisan config:cache
+php artisan route:cache
 
 # Decrypt the OAuth keys.
 php artisan ck:decrypt-oauth-keys \
---public-key-name=oauth-public-production.key \
---private-key-name=oauth-private-production.key
+    --public-key-name=oauth-public-production.key \
+    --private-key-name=oauth-private-production.key
