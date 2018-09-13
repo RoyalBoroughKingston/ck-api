0948bbc4938a09b4bf812e91ca66b76158daa7f3
Matt Inamdar <matt@ayup.agency>
2018-09-13 22:37:58 +0100

    added scheduler and queue worker scripts

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -6,11 +6,11 @@ SECRET_ID=ck-api-env-production
 SECRET_REGION=eu-west-1
 
 # Go to the install directory.
-cd $DOCUMENT_ROOT
+cd ${DOCUMENT_ROOT}
 
 # Download the .env file from AWS Secrets Manager.
 echo "Importing .env file..."
-aws secretsmanager get-secret-value --secret-id $SECRET_ID --region $SECRET_REGION | \
+aws secretsmanager get-secret-value --secret-id ${SECRET_ID} --region ${SECRET_REGION} | \
     python -c "import json,sys;obj=json.load(sys.stdin);print obj['SecretString'];" > .env
 
 # Install NPM dependencies.
