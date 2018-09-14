f13f13558c16c1496b56b631bec10681529c22af
Matt Inamdar <matt@ayup.agency>
2018-09-14 15:19:58 +0100

    updated supervisor

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
 if [ ${APP_ROLE} == "queue-worker" ]; then
-    # Go to the temp directory.
-    cd /tmp
     # Create the supervisor configuration file for the Laravel queue worker.
-    sudo touch /var/log/queue-worker.log
     curl https://raw.githubusercontent.com/RoyalBoroughKingston/ck-scripts/master/queue-worker/laravel-worker.conf | sudo tee /etc/supervisor/conf.d/laravel-worker.conf
 
     # Restart supervisor.
-    sudo service supervisord stop
-    sudo service supervisord start
+    sudo supervisorctl reread
+    sudo supervisorctl start laravel-worker:*
 fi
