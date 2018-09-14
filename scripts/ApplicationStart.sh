4ad3d69b3e56601656165672f0750f91dfd20858
Matt Inamdar <matt@ayup.agency>
2018-09-14 17:23:46 +0100

    set travis to build on master and develop only

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ "$APP_ROLE" = "queue-worker" ]; then
+if [ -d "/etc/supervisor" ]; then
     # Create the supervisor configuration file for the Laravel queue worker.
     curl https://raw.githubusercontent.com/RoyalBoroughKingston/ck-scripts/master/queue-worker/laravel-worker.conf | sudo tee /etc/supervisor/conf.d/laravel-worker.conf
 
