6ad50ccc329ce6018a373ea5d2acd4a6e30d87bd
Matt Inamdar <matt@ayup.agency>
2018-09-17 16:14:25 +0100

    added user created email tests

---
+++ tests/Unit/Listeners/Notifications/UserCreatedTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Unit\Listeners\Notifications;
+
+use App\Emails\UserCreated\NotifyUserEmail;
+use App\Events\EndpointHit;
+use App\Listeners\Notifications\UserCreated;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Queue;
+use Tests\TestCase;
+
+class UserCreatedTest extends TestCase
+{
+    public function test_emails_sent_out()
+    {
+        Queue::fake();
+
+        $user = factory(User::class)->create();
+
+        $request = Request::create('')->setUserResolver(function () {
+            return factory(User::class)->create();
+        });
+        $event = EndpointHit::onCreate($request, '', $user);
+        $listener = new UserCreated();
+        $listener->handle($event);
+
+        Queue::assertPushedOn('notifications', NotifyUserEmail::class);
+    }
+}
