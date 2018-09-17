d6893ed5df67ac2232c27e292338a6f9c89c1a01
Matt Inamdar <matt@ayup.agency>
2018-09-17 12:44:20 +0100

    updated Email / User Permissions Updated / Notify User

--- app/Listeners/Notifications/UserRolesUpdated.php
+++ app/Listeners/Notifications/UserRolesUpdated.php
@@ -4,6 +4,8 @@ namespace App\Listeners\Notifications;
 
 use App\Emails\UserRolesUpdated\NotifyUserEmail;
 use App\Events\UserRolesUpdated as UserRolesUpdatedEvent;
+use App\Models\Role;
+use App\Models\UserRole;
 
 class UserRolesUpdated
 {
@@ -23,10 +25,37 @@ class UserRolesUpdated
      */
     protected function notifyUser(UserRolesUpdatedEvent $event)
     {
+        $humanReadableRole = function (UserRole $userRole) {
+            switch ($userRole->role_id) {
+                case Role::superAdmin()->id:
+                    return 'Super admin';
+                case Role::globalAdmin()->id:
+                    return 'Global admin';
+                case Role::organisationAdmin()->id:
+                    return "Organisation admin for {$userRole->organisation->name}";
+                case Role::serviceAdmin()->id:
+                    return "Service admin for {$userRole->service->name}";
+                case Role::serviceWorker()->id:
+                    return "Service worker for {$userRole->service->name}";
+                default:
+                    return 'Unknown role';
+            }
+        };
+
+        $oldRoles = $event->oldRoles
+            ->load('role', 'organisation', 'service')
+            ->map($humanReadableRole)
+            ->implode(', ');
+
+        $newRoles = $event->newRoles
+            ->load('role', 'organisation', 'service')
+            ->map($humanReadableRole)
+            ->implode(', ');
+
         $event->user->sendEmail(new NotifyUserEmail($event->user->email, [
             'NAME' => $event->user->first_name,
-            'OLD_PERMISSIONS' => $event->oldRoles->toJson(),
-            'NEW_PERMISSIONS' => $event->newRoles->toJson(),
+            'OLD_PERMISSIONS' => $oldRoles,
+            'PERMISSIONS' => $newRoles,
         ]));
     }
 }
