e734bde9f7a97edda8c31fb0bea50a14838bd551
Matthew Inamdar <13454566+matthew-inamdar@users.noreply.github.com>
2018-09-18 15:16:18 +0100

    fixed truthy values being passed (#114)

--- app/Listeners/Notifications/UpdateRequestRejected.php
+++ app/Listeners/Notifications/UpdateRequestRejected.php
@@ -43,7 +43,7 @@ class UpdateRequestRejected
             $resourceName = $updateRequest->updateable->name;
             $resourceType = 'service';
         } elseif ($updateRequest->updateable instanceof ServiceLocation) {
-            $resourceName = $updateRequest->updateable->name || $updateRequest->updateable->id;
+            $resourceName = $updateRequest->updateable->name ?? $updateRequest->updateable->location->address_line_1;
             $resourceType = 'service location';
         } elseif ($updateRequest->updateable instanceof Organisation) {
             $resourceName = $updateRequest->updateable->name;
