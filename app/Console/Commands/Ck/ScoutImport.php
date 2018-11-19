ec717e82e8ba7480ba05b5b8ab45c1b63ddf6902
Matthew Inamdar <matthew.inamdar@goldspecdigital.com>
2018-11-19 15:10:00 +0000

    added scout import command

---
+++ app/Console/Commands/Ck/ScoutImport.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Console\Commands\Ck;
+
+use Laravel\Scout\Console\ImportCommand;
+
+class ScoutImport extends ImportCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ck:scout-import {model}';
+}
