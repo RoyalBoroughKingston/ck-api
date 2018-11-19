ff8eed2807a4d8f5b2c1c6da509fcc0de95fd405
Matthew Inamdar <matthew.inamdar@goldspecdigital.com>
2018-11-19 10:01:47 +0000

    added local cloud disk for dev

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,6 +48,11 @@ return [
             'root' => storage_path('app'),
         ],
 
+        'local_cloud' => [
+            'driver' => 'local',
+            'root' => storage_path('cloud'),
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
