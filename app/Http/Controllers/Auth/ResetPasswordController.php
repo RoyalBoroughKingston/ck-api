ebb4c353f6672e850355da879ec86f04d01072bd
Matthew Inamdar <13454566+matthew-inamdar@users.noreply.github.com>
2018-11-23 13:36:55 +0000

    added logic to require OTP to login after password reset (#158)

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Rules\Password;
+use App\Sms\OtpLoginCode\UserSms;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
 
 class ResetPasswordController extends Controller
 {
@@ -50,5 +52,38 @@ class ResetPasswordController extends Controller
             'email' => ['required', 'email'],
             'password' => ['required', 'confirmed', new Password()],
         ];
+    }
+
+    /**
+     * Get the response for a successful password reset.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $response
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    protected function sendResetResponse(Request $request, $response)
+    {
+        // If OTP is disabled then skip this method.
+        if (!config('ck.otp_enabled')) {
+            return redirect($this->redirectPath())
+                ->with('status', trans($response));
+        }
+
+        // Get the user.
+        $user = $this->guard()->user();
+
+        // Log user out.
+        $this->guard()->logout();
+
+        // Place the user ID in the session.
+        session()->put('otp.user_id', $user->id);
+
+        // Generate and send the OTP code.
+        $otpCode = mt_rand(10000, 99999);
+        session()->put('otp.code', $otpCode);
+        $user->sendSms(new UserSms($user->phone, ['OTP_CODE' => $otpCode]));
+
+        // Forward the user to the code page.
+        return redirect('/login/code');
     }
 }
