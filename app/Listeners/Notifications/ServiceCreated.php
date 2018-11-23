85d52cdaea5fd5acbba14cf7342c916d2161c109
Matthew Inamdar <13454566+matthew-inamdar@users.noreply.github.com>
2018-11-23 12:07:57 +0000

    Feature/create service process (#157)
    * added logic to send the email
    
    * email only sent when created by OA
    
    * added service intro to notification email

---
+++ app/Listeners/Notifications/ServiceCreated.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Listeners\Notifications;
+
+use App\Emails\ServiceCreated\NotifyGlobalAdminEmail;
+use App\Events\EndpointHit;
+use App\Models\Audit;
+use App\Models\Notification;
+use App\Models\Service;
+use App\Models\User;
+
+class ServiceCreated
+{
+    /**
+     * Handle the event.
+     *
+     * @param  EndpointHit $event
+     * @return void
+     */
+    public function handle(EndpointHit $event)
+    {
+        // Only handle specific endpoint events.
+        if ($event->isntFor(Service::class, Audit::ACTION_CREATE)) {
+            return;
+        }
+
+        // Only send the notification if the user who created the service is not a global admin.
+        if (!$event->getUser()->isGlobalAdmin()) {
+            $this->notifyGlobalAdmins($event->getModel(), $event->getUser());
+        }
+    }
+
+    /**
+     * @param \App\Models\Service $service
+     * @param \App\Models\User $user
+     */
+    protected function notifyGlobalAdmins(Service $service, User $user)
+    {
+        Notification::sendEmail(
+            new NotifyGlobalAdminEmail(config('ck.global_admin.email'), [
+                'SERVICE_NAME' => $service->name,
+                'ORGANISATION_ADMIN_NAME' => $user->full_name,
+                'SERVICE_INTRO' => $service->intro,
+                'ORGANISATION_NAME' => $service->organisation->name,
+                'ORGANISATION_ADMIN_EMAIL' => $user->email,
+                'SERVICE_URL' => backend_uri("/services/{$service->id}"),
+            ])
+        );
+    }
+}
