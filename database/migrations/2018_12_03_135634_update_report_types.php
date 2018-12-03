5a5a164fb9eb8c3885dd208f3c79f79f68bf2f63
Matt Inamdar <matt@ayup.agency>
2018-12-03 14:04:41 +0000

    updated report types in database

---
+++ database/migrations/2018_12_03_135634_update_report_types.php
@@ -0,0 +1,101 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class UpdateReportTypes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('report_types')->delete();
+
+        $now = now();
+
+        DB::table('report_types')->insert([
+            'id' => uuid(),
+            'name' => 'Users Export',
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        DB::table('report_types')->insert([
+            'id' => uuid(),
+            'name' => 'Services Export',
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        DB::table('report_types')->insert([
+            'id' => uuid(),
+            'name' => 'Organisations Export',
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        DB::table('report_types')->insert([
+            'id' => uuid(),
+            'name' => 'Locations Export',
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        DB::table('report_types')->insert([
+            'id' => uuid(),
+            'name' => 'Referrals Export',
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        DB::table('report_types')->insert([
+            'id' => uuid(),
+            'name' => 'Feedback Export',
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        DB::table('report_types')->insert([
+            'id' => uuid(),
+            'name' => 'Audit Logs Export',
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        DB::table('report_types')->insert([
+            'id' => uuid(),
+            'name' => 'Search Histories Export',
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        DB::table('report_types')->insert([
+            'id' => uuid(),
+            'name' => 'Thesaurus Export',
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table('report_types')->delete();
+
+        $now = now();
+
+        DB::table('report_types')->insert([
+            'id' => uuid(),
+            'name' => 'Commissioners Report',
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+    }
+}
