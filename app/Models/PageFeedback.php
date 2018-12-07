ea25cf5a71b36595dfd2e613481ef3059c04542f
Matthew Inamdar <matthew.inamdar@goldspecdigital.com>
2018-12-07 17:03:31 +0000

    added migration

--- app/Models/PageFeedback.php
+++ app/Models/PageFeedback.php
@@ -11,4 +11,15 @@ class PageFeedback extends Model
     use PageFeedbackMutators;
     use PageFeedbackRelationships;
     use PageFeedbackScopes;
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'consented_at' => 'datetime',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
 }
