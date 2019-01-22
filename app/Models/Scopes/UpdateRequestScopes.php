89b294c0b6b3500d79bc046e6f2a1312fc552406
Matt Inamdar <matt@ayup.agency>
2019-01-22 16:07:51 +0000

    Added entry to update request resource

--- app/Models/Scopes/UpdateRequestScopes.php
+++ app/Models/Scopes/UpdateRequestScopes.php
@@ -83,26 +83,26 @@ trait UpdateRequestScopes
         return <<< EOT
 CASE `update_requests`.`updateable_type`
     WHEN "services" THEN (
-        SELECT `update_requests`.`data`->>"$.name"
+        SELECT `services`.`name`
         FROM `services`
         WHERE `update_requests`.`updateable_id` = `services`.`id`
         LIMIT 1
     )
     WHEN "locations" THEN (
-        SELECT `update_requests`.`data`->>"$.address_line_1"
+        SELECT `locations`.`address_line_1`
         FROM `locations`
         WHERE `update_requests`.`updateable_id` = `locations`.`id`
         LIMIT 1
     )
     WHEN "service_locations" THEN (
-        SELECT IF(`update_requests`.`data`->>"$.name" = "null", `locations`.`address_line_1`, `update_requests`.`data`->>"$.name")
+        SELECT IFNULL(`service_locations`.`name`, `locations`.`address_line_1`)
         FROM `service_locations`
         LEFT JOIN `locations` ON `service_locations`.`location_id` = `locations`.`id`
         WHERE `update_requests`.`updateable_id` = `service_locations`.`id`
         LIMIT 1
     )
     WHEN "organisations" THEN (
-        SELECT `update_requests`.`data`->>"$.name"
+        SELECT `organisations`.`name`
         FROM `organisations`
         WHERE `update_requests`.`updateable_id` = `organisations`.`id`
         LIMIT 1
