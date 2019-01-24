8182f5a9d8a6e4a704560684a005a3fbb0a549f5
Matt Inamdar <matt@ayup.agency>
2019-01-24 09:43:26 +0000

    Added test to make sure only completed referrals due for deletion are deleted

--- tests/Unit/Console/Commands/Ck/AutoDeleteReferralsTest.php
+++ tests/Unit/Console/Commands/Ck/AutoDeleteReferralsTest.php
@@ -32,4 +32,19 @@ class AutoDeleteReferralsTest extends TestCase
         $this->assertDatabaseHas($newReferral->getTable(), ['id' => $newReferral->id]);
         $this->assertDatabaseMissing($dueForDeletionReferral->getTable(), ['id' => $dueForDeletionReferral->id]);
     }
+
+    public function test_old_incompleted_referrals_are_not_deleted()
+    {
+        $dueForDeletionReferral = factory(Referral::class)->create([
+            'email' => 'test@example.com',
+            'referee_email' => 'test@example.com',
+            'status' => Referral::STATUS_INCOMPLETED,
+            'created_at' => today()->subMonths(Referral::AUTO_DELETE_MONTHS),
+            'updated_at' => today()->subMonths(Referral::AUTO_DELETE_MONTHS),
+        ]);
+
+        Artisan::call(AutoDeleteReferralsCommand::class);
+
+        $this->assertDatabaseHas($dueForDeletionReferral->getTable(), ['id' => $dueForDeletionReferral->id]);
+    }
 }
