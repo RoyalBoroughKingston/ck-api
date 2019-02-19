e99d70a3add108e1b5b2ea551dd9a17a3a9698a8
Matt Inamdar <matt@ayup.agency>
2019-02-19 16:45:45 +0000

    Created form request class for image routes

---
+++ app/Http/Requests/ImageFormRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class ImageFormRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public final function rules()
+    {
+        $rules = [
+            'max_dimension' => ['integer', 'min:1', 'max:1000'],
+        ];
+
+        return array_merge($rules, $this->extraRules());
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    abstract protected function extraRules(): array;
+}
