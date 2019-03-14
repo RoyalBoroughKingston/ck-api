3f483180ec78c287f7247f74332ddbf3cafa2dff
Matthew Inamdar <matthew@inamdar.co.uk>
2019-03-14 14:18:21 +0000

    Feature/global admins can add organisation taxonomies (#208)
    * Updated API docs
    
    * Updated permissions

--- app/Http/Requests/TaxonomyOrganisation/StoreRequest.php
+++ app/Http/Requests/TaxonomyOrganisation/StoreRequest.php
@@ -14,7 +14,7 @@ class StoreRequest extends FormRequest
      */
     public function authorize()
     {
-        if ($this->user()->isSuperAdmin()) {
+        if ($this->user()->isGlobalAdmin()) {
             return true;
         }
 
