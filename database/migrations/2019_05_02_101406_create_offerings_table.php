3f6a25d6a72db2ca03f317d731a45bb0ba6a69e3
Matt Inamdar <matt@ayup.agency>
2019-05-02 11:17:56 +0100

    Added offerings migration

---
+++ database/migrations/2019_05_02_101406_create_offerings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOfferingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offerings', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('service_id');
+            $table->foreign('service_id')->references('id')->on('services');
+            $table->text('offering');
+            $table->unsignedInteger('order');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offerings');
+    }
+}
