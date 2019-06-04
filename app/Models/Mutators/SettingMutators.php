442e0c0755e0da43cd9f1a82d36c8dc8218c78f4
Matt Inamdar <matt@ayup.agency>
2019-06-04 16:01:45 +0100

    Added mutators for setting model

--- app/Models/Mutators/SettingMutators.php
+++ app/Models/Mutators/SettingMutators.php
@@ -4,5 +4,20 @@ namespace App\Models\Mutators;
 
 trait SettingMutators
 {
-    //
+    /**
+     * @param string $value
+     * @return mixed
+     */
+    public function getValueAttribute(string $value)
+    {
+        return json_decode($value, true);
+    }
+
+    /**
+     * @param mixed $value
+     */
+    public function setValueAttribute($value): void
+    {
+        $this->attributes['value'] = json_encode($value);
+    }
 }
