39793eb39daf34701e258c6ab6cad60f0df51be1
Matt Inamdar <matt@ayup.agency>
2019-06-07 17:10:04 +0100

    feature/Search-Histories-Export-Missing-Data (#240)

--- app/Models/Scopes/SearchHistoryScopes.php
+++ app/Models/Scopes/SearchHistoryScopes.php
@@ -2,7 +2,19 @@
 
 namespace App\Models\Scopes;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\DB;
+
 trait SearchHistoryScopes
 {
-    //
+    /**
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeWithFilledQuery(Builder $query): Builder
+    {
+        return $query->whereNotNull(
+            DB::raw('JSON_EXTRACT(`query`, "$.query.bool.must.bool.should[0].match.name.query")')
+        );
+    }
 }
