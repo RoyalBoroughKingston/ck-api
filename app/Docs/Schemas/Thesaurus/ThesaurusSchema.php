d812e3e612363b73c20cc187b8d7ce71f3e0ed47
Matt Inamdar <matt@ayup.agency>
2019-07-15 15:25:01 +0100

    Added thesaurus schemas

---
+++ app/Docs/Schemas/Thesaurus/ThesaurusSchema.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Docs\Schemas\Thesaurus;
+
+use GoldSpecDigital\ObjectOrientedOAS\Objects\BaseObject;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Schema;
+
+class ThesaurusSchema extends Schema
+{
+    /**
+     * @param string|null $objectId
+     * @return static
+     */
+    public static function create(string $objectId = null): BaseObject
+    {
+        return parent::create($objectId)
+            ->type(static::TYPE_ARRAY)
+            ->items(Schema::string());
+    }
+}
