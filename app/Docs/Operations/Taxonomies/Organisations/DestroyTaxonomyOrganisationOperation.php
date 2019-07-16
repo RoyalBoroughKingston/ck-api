875c210b59ef0f67228a6bdaa96ba4713298d9d1
Matt Inamdar <matt@ayup.agency>
2019-07-16 14:18:17 +0100

    Added taxonomy organisation operations

---
+++ app/Docs/Operations/Taxonomies/Organisations/DestroyTaxonomyOrganisationOperation.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Docs\Operations\Taxonomies\Organisations;
+
+use App\Docs\Responses\ResourceDeletedResponse;
+use App\Docs\Tags\TaxonomyOrganisationsTag;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\BaseObject;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Operation;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Parameter;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Schema;
+
+class DestroyTaxonomyOrganisationOperation extends Operation
+{
+    /**
+     * @param string|null $objectId
+     * @throws \GoldSpecDigital\ObjectOrientedOAS\Exceptions\InvalidArgumentException
+     * @return static
+     */
+    public static function create(string $objectId = null): BaseObject
+    {
+        return parent::create($objectId)
+            ->action(static::ACTION_DELETE)
+            ->tags(TaxonomyOrganisationsTag::create())
+            ->summary('Delete a specific organisation taxonomy')
+            ->description('**Permission:** `Super Admin`')
+            ->parameters(
+                Parameter::path()
+                    ->name('organisation')
+                    ->description('The ID of the organisation taxonomy')
+                    ->required()
+                    ->schema(Schema::string()->format(Schema::FORMAT_UUID))
+            )
+            ->responses(
+                ResourceDeletedResponse::create(null, 'taxonomy organisation')
+            );
+    }
+}
---
+++ app/Docs/Operations/Taxonomies/Organisations/ShowTaxonomyOrganisationOperation.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Docs\Operations\Taxonomies\Organisations;
+
+use App\Docs\Schemas\ResourceSchema;
+use App\Docs\Schemas\Taxonomy\Organisation\TaxonomyOrganisationSchema;
+use App\Docs\Tags\TaxonomyOrganisationsTag;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\BaseObject;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\MediaType;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Operation;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Parameter;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Response;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Schema;
+
+class ShowTaxonomyOrganisationOperation extends Operation
+{
+    /**
+     * @param string|null $objectId
+     * @throws \GoldSpecDigital\ObjectOrientedOAS\Exceptions\InvalidArgumentException
+     * @return static
+     */
+    public static function create(string $objectId = null): BaseObject
+    {
+        return parent::create($objectId)
+            ->action(static::ACTION_GET)
+            ->tags(TaxonomyOrganisationsTag::create())
+            ->summary('Get a specific organisation taxonomy')
+            ->description('**Permission:** `Open`')
+            ->noSecurity()
+            ->parameters(
+                Parameter::path()
+                    ->name('organisation')
+                    ->description('The ID of the organisation taxonomy')
+                    ->required()
+                    ->schema(Schema::string()->format(Schema::FORMAT_UUID))
+            )
+            ->responses(
+                Response::ok()->content(
+                    MediaType::json()->schema(
+                        ResourceSchema::create(null, TaxonomyOrganisationSchema::create())
+                    )
+                )
+            );
+    }
+}
