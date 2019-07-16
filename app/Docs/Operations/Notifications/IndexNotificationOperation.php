65d8412156afa4b7a9e8dd41e2cd52c68a1a68d5
Matt Inamdar <matt@ayup.agency>
2019-07-16 10:42:43 +0100

    Added notification operations

---
+++ app/Docs/Operations/Notifications/IndexNotificationOperation.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Docs\Operations\Notifications;
+
+use App\Docs\Parameters\FilterIdParameter;
+use App\Docs\Parameters\FilterParameter;
+use App\Docs\Parameters\PageParameter;
+use App\Docs\Parameters\PerPageParameter;
+use App\Docs\Schemas\Notification\NotificationSchema;
+use App\Docs\Schemas\PaginationSchema;
+use App\Docs\Tags\NotificationsTag;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\BaseObject;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\MediaType;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Operation;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Response;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Schema;
+
+class IndexNotificationOperation extends Operation
+{
+    /**
+     * @param string|null $objectId
+     * @throws \GoldSpecDigital\ObjectOrientedOAS\Exceptions\InvalidArgumentException
+     * @return static
+     */
+    public static function create(string $objectId = null): BaseObject
+    {
+        return parent::create($objectId)
+            ->action(static::ACTION_GET)
+            ->tags(NotificationsTag::create())
+            ->summary('List all the notifications')
+            ->description(
+<<<EOT
+**Permission:** `Global Admin`
+
+---
+
+Notifications are returned in descending order of the date they were created.
+EOT
+            )
+            ->parameters(
+                PageParameter::create(),
+                PerPageParameter::create(),
+                FilterIdParameter::create(),
+                FilterParameter::create(null, 'referral_id')
+                    ->description('Comma separated list of referral IDs to filter by')
+                    ->schema(Schema::string()),
+                FilterParameter::create(null, 'service_id')
+                    ->description('Comma separated list of service IDs to filter by')
+                    ->schema(Schema::string()),
+                FilterParameter::create(null, 'user_id')
+                    ->description('Comma separated list of user IDs to filter by')
+                    ->schema(Schema::string())
+            )
+            ->responses(
+                Response::ok()->content(
+                    MediaType::json()->schema(
+                        PaginationSchema::create(null, NotificationSchema::create())
+                    )
+                )
+            );
+    }
+}
---
+++ app/Docs/Operations/Notifications/ShowNotificationOperation.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Docs\Operations\Notifications;
+
+use App\Docs\Schemas\Notification\NotificationSchema;
+use App\Docs\Tags\NotificationsTag;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\BaseObject;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\MediaType;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Operation;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Parameter;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Response;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Schema;
+
+class ShowNotificationOperation extends Operation
+{
+    /**
+     * @param string|null $objectId
+     * @throws \GoldSpecDigital\ObjectOrientedOAS\Exceptions\InvalidArgumentException
+     * @return static
+     */
+    public static function create(string $objectId = null): BaseObject
+    {
+        return parent::create($objectId)
+            ->action(static::ACTION_GET)
+            ->tags(NotificationsTag::create())
+            ->summary('Get a specific notification')
+            ->description('**Permission:** `Global Admin`')
+            ->parameters(
+                Parameter::path()
+                    ->name('notification')
+                    ->description('The ID of the notification')
+                    ->required()
+                    ->schema(Schema::string()->format(Schema::FORMAT_UUID))
+            )
+            ->responses(
+                Response::ok()->content(
+                    MediaType::json()->schema(NotificationSchema::create())
+                )
+            );
+    }
+}
