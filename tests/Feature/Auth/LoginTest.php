304a889048934c46846e4b9018fb216a6569c40d
Stuart Laverick <stuart@appropriatesolutions.co.uk>
2023-01-17 11:12:40 +0000

    Middleware to prevent referrals during maintenance

--- tests/Feature/Auth/LoginTest.php
+++ tests/Feature/Auth/LoginTest.php
@@ -2,11 +2,12 @@
 
 namespace Tests\Feature\Auth;
 
-use App\Models\User;
-use App\Sms\OtpLoginCode\UserSms;
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\Queue;
 use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Http\Response;
+use App\Sms\OtpLoginCode\UserSms;
+use Illuminate\Support\Facades\Queue;
+use Illuminate\Support\Facades\Config;
 
 class LoginTest extends TestCase
 {
@@ -28,5 +29,28 @@ class LoginTest extends TestCase
             $this->assertArrayHasKey('OTP_CODE', $sms->values);
             return true;
         });
+    }
+
+    /**
+    * @test
+    */
+    public function loginWhenApplicationisDownForMaintenance503()
+    {
+        $this->artisan('down');
+
+        $user = factory(User::class)->create(['password' => bcrypt('password')]);
+        factory(\App\Models\Service::class)->create();
+
+        // Login is prevented
+        $this->get(route('login'))->assertStatus(Response::HTTP_SERVICE_UNAVAILABLE);
+        $this->post(route('login'), [
+            'email' => $user->email,
+            'password' => 'password',
+        ])->assertStatus(Response::HTTP_SERVICE_UNAVAILABLE);
+
+        // Other endpoints are available
+        $this->json('GET', '/core/v1/services')->assertStatus(Response::HTTP_OK);
+
+        $this->artisan('up');
     }
 }
